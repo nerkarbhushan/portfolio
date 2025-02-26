e9e0e7403d2cd7d1bfa5cbd211d1916bba520da2
Bhushan Nerkar <nerkarbhushan.b@gmail.com>
2025-02-26 12:32:05 +0530

    Update linksConfig.js

--- src/config/linksConfig.js
+++ src/config/linksConfig.js
@@ -1,6 +1,6 @@
 export const links = {
   resume:
-    "https://drive.google.com/file/d/1eCp-g2cauvEx71ceGLOOOqLgLpoh98YV/view?usp=drive_link",
+    "https://drive.google.com/file/d/1wXOl08CATmCMuPkofJNv3TWzK3JMzyhv/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/nerkar-bhushan-bapurao/",
   github: "https://github.com/nerkarbhushan",
   leetcode: "https://leetcode.com/u/nerkarbhushan_b/",
