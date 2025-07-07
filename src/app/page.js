9c78866a44ab31732b8efaa22c2c473aab49001c
Bhushan Nerkar <nerkarbhushan.b@gmail.com>
2025-07-07 15:45:23 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,23 @@
-import AboutSection from '@/components/AboutSection';
-import ContactSection from '@/components/ContactSection';
-import ExperienceSection from '@/components/ExperienceSection';
-import HeroSection from '@/components/HeroSection';
-import NavBar from '@/components/NavBar';
+import AboutSection from "@/components/AboutSection";
+import CertificationsSection from "@/components/CertificationsSection ";
+import ContactSection from "@/components/ContactSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import HeroSection from "@/components/HeroSection";
+import NavBar from "@/components/NavBar";
+import ProjectsSection from "@/components/ProjectsSection";
+import SkillsSection from "@/components/SkillsSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className='container mx-auto mt-4 px-6 md:px-12 py-4'>
+      <div className="container mx-auto mt-4 px-6 md:px-12 py-4">
         <NavBar />
         <HeroSection />
         <AboutSection />
+        <SkillsSection />
         <ExperienceSection />
+        <ProjectsSection />
+        <CertificationsSection />
         <ContactSection />
       </div>
     </main>
